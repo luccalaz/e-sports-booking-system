20908e93ca9540fe2928d0c43fa135c2198a65e4
luccalaz <luccalazza@gmail.com>
2025-01-31 15:00:44 -0400

    Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
 import LogoutButton from "@/components/logout-button";
 
 export default async function Home() {
